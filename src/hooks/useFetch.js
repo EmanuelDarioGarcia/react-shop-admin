e5f2b4dec745876b7e0bd56764ff3bcee0480f8a
EmanuelDarioGarcia <pl5782100@gmail.com>
2023-05-16 14:09:11 -0300

    Proyecto de inicio de sesion y dashboard

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,21 @@
+import { useEffect, useState} from 'react';
+import axios from 'axios';
+import { set } from 'lodash';
+
+const useFetch = (endPoints) => {
+    const [data, setData] = useState([]);
+    async function fetchData() {
+        const response = await axios.get(endPoints);
+        setData(response.data);
+    }
+    useEffect(() => {
+        try { 
+        fetchData();
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+    return data;
+}
+
+export default useFetch;
